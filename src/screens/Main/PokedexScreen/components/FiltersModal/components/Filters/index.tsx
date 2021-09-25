3ac38bc1a82a0620932c982612db2da8fa85af74
Bryant dos Anjos <brilliant@pop-os.localdomain>
2021-09-25 00:58:58 -0300

    feat: created filters modal layout

---
+++ src/screens/Main/PokedexScreen/components/FiltersModal/components/Filters/index.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { View, TouchableOpacity } from 'react-native'
+import { Button, IconButton, Text } from 'react-native-paper'
+
+import styles from './styles'
+import Types from './components/Types'
+
+type Props = {
+  closeModal?(): void
+}
+
+const Filters = (props: Props) => {
+  const { closeModal } = props
+
+  return (
+    <View style={styles.wrapper}>
+      <View style={styles.top}>
+        <View style={styles.header}>
+          <View style={styles.row}>
+            <Text style={styles.title}>Filtro</Text>
+            <TouchableOpacity onPress={() => console.log('pressed')}>
+              <Text style={styles.clean}>Limpar filtros</Text>
+            </TouchableOpacity>
+          </View>
+
+          <IconButton icon="close" onPress={closeModal} />
+        </View>
+
+        <View style={{ flex: 1 }}>
+          <Text style={styles.subtitle}>Tipo</Text>
+          <Types />
+        </View>
+      </View>
+
+      <View style={styles.bottom}>
+        <Button
+          mode="contained"
+          style={styles.button}
+          labelStyle={styles.buttonText}
+          onPress={closeModal}
+        >
+          Aplicar
+        </Button>
+      </View>
+    </View>
+  )
+}
+
+export default Filters
---
+++ src/screens/Main/PokedexScreen/components/FiltersModal/components/Filters/styles.ts
@@ -0,0 +1,49 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    padding: 20,
+  },
+  top: {
+    flex: 0.9,
+  },
+  bottom: {
+    flex: 0.1,
+    justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 40,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginRight: 11,
+  },
+  clean: {
+    color: '#4A7DFF',
+    textDecorationLine: 'underline',
+  },
+  subtitle: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#3F3F3F',
+  },
+  button: {
+    elevation: 0,
+    padding: 5,
+  },
+  buttonText: {
+    textTransform: 'capitalize',
+    fontWeight: '600',
+  },
+})
+
+export default styles
