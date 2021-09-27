7311e99ece0ddff5c0d52d82dd1147f518af0da3
Bryant Moreira dos Anjos <bryant_mx@hotmail.com>
2021-09-27 12:29:42 -0300

    feat: did the evolution chain work with the most of cases

--- src/screens/Main/PokemonScreen/components/Infos/components/Evolution/styles.ts
+++ src/screens/Main/PokemonScreen/components/Infos/components/Evolution/styles.ts
@@ -5,37 +5,48 @@ const { width } = Dimensions.get('window')
 const size = width / 4
 
 const styles = StyleSheet.create({
-  wrapper: {
+  container: {
+    width: '100%',
+    alignSelf: 'center',
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
     alignItems: 'center',
   },
-  childWrapper: {
-    flexDirection: 'column',
+  icon: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  container: {
+  gradient: {
+    alignItems: 'center',
+    justifyContent: 'center',
     height: size,
     width: size,
     borderRadius: size,
     marginVertical: 10,
-    overflow: 'visible',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  wrapper: {
+    height: size,
+    width: size,
+    borderRadius: size,
+    overflow: 'hidden',
   },
   image: {
+    position: 'absolute',
     width: '80%',
     height: '80%',
-    bottom: 10,
+    top: -10,
   },
   label: {
     color: Colors.white,
     fontWeight: 'bold',
     textTransform: 'capitalize',
-    bottom: 10,
+    bottom: 15,
   },
   touchable: {
     width: '100%',
     height: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
 })
 
